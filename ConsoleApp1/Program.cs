37ef2d2a53b78bc96862dafaadd9d57c63c1db27
Artem <74129435+ArtemTsy@users.noreply.github.com>
2020-11-17 17:26:43 +0700

    Add files via upload

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -6,7 +11,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String word = "";
+            String[] wordMass;
+            Console.WriteLine("Enter the path");
+            string path_name = Console.ReadLine();
+            StreamReader sr = new StreamReader(path_name);
+
+            if (path_name.Contains(".txt"))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    word += sr.ReadLine();
+                }
+                wordMass = word.Split(" ");
+                Console.WriteLine("Количество слов:");
+                Console.WriteLine(wordMass.Length);
+            }
+            else
+            {
+                Console.WriteLine("Неверный тип файла");
+            }
+            sr.Close();
+            Console.ReadLine();
+
         }
     }
 }
+
